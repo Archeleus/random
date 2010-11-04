0d9e1ed399ae3795ee10a12dec97f8316810e569
Alex <alex@archeleus.com>
2010-11-04 16:22:50 +0800

    New file in EXR project.
    Libraries succesfully found. Project settings correct.

---
+++ EXRstuff/Debug/main.d
@@ -0,0 +1,62 @@
+main.d main.o: ../main.cpp /usr/include/OpenEXR/ImfInputFile.h \
+ /usr/include/OpenEXR/ImfHeader.h /usr/include/OpenEXR/ImfLineOrder.h \
+ /usr/include/OpenEXR/ImfCompression.h /usr/include/OpenEXR/ImfName.h \
+ /usr/include/OpenEXR/ImfTileDescription.h \
+ /usr/include/OpenEXR/ImfInt64.h /usr/include/OpenEXR/ImathInt64.h \
+ /usr/include/OpenEXR/ImathVec.h /usr/include/OpenEXR/ImathExc.h \
+ /usr/include/OpenEXR/IexBaseExc.h /usr/include/OpenEXR/ImathLimits.h \
+ /usr/include/OpenEXR/ImathMath.h /usr/include/OpenEXR/ImathPlatform.h \
+ /usr/include/OpenEXR/ImathBox.h /usr/include/OpenEXR/ImfFrameBuffer.h \
+ /usr/include/OpenEXR/ImfPixelType.h \
+ /usr/include/OpenEXR/ImfTiledOutputFile.h \
+ /usr/include/OpenEXR/ImfThreading.h /usr/include/OpenEXR/ImfRgbaFile.h \
+ /usr/include/OpenEXR/ImfRgba.h /usr/include/OpenEXR/half.h \
+ /usr/include/OpenEXR/ImfChannelList.h /usr/include/OpenEXR/half.h
+
+/usr/include/OpenEXR/ImfInputFile.h:
+
+/usr/include/OpenEXR/ImfHeader.h:
+
+/usr/include/OpenEXR/ImfLineOrder.h:
+
+/usr/include/OpenEXR/ImfCompression.h:
+
+/usr/include/OpenEXR/ImfName.h:
+
+/usr/include/OpenEXR/ImfTileDescription.h:
+
+/usr/include/OpenEXR/ImfInt64.h:
+
+/usr/include/OpenEXR/ImathInt64.h:
+
+/usr/include/OpenEXR/ImathVec.h:
+
+/usr/include/OpenEXR/ImathExc.h:
+
+/usr/include/OpenEXR/IexBaseExc.h:
+
+/usr/include/OpenEXR/ImathLimits.h:
+
+/usr/include/OpenEXR/ImathMath.h:
+
+/usr/include/OpenEXR/ImathPlatform.h:
+
+/usr/include/OpenEXR/ImathBox.h:
+
+/usr/include/OpenEXR/ImfFrameBuffer.h:
+
+/usr/include/OpenEXR/ImfPixelType.h:
+
+/usr/include/OpenEXR/ImfTiledOutputFile.h:
+
+/usr/include/OpenEXR/ImfThreading.h:
+
+/usr/include/OpenEXR/ImfRgbaFile.h:
+
+/usr/include/OpenEXR/ImfRgba.h:
+
+/usr/include/OpenEXR/half.h:
+
+/usr/include/OpenEXR/ImfChannelList.h:
+
+/usr/include/OpenEXR/half.h:
---
+++ EXRstuff/main.cpp
@@ -0,0 +1,14 @@
+
+#include <ImfInputFile.h>
+#include <ImfRgbaFile.h>
+#include <ImfChannelList.h>
+#include <ImfFrameBuffer.h>
+#include <half.h>
+#include <cstdio>
+using namespace Imf;
+using namespace Imath;
+
+int main(void){
+printf("OpenEXR\n");
+return 0;
+}
